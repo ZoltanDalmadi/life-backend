91f60cdf834545860ba09ae805a16e916069ae4a
Zoltan Dalmadi <dmz985@gmail.com>
2017-02-20 10:51:31 +0100

    Initial commit

---
+++ src/main/java/com/github/life/LifeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.github.life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeBackendApplication.class, args);
+	}
+}
