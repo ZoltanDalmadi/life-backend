89ae1dc0ed1f828acdeaa1e25ac40b8994c27550
Zoltan Dalmadi <dmz985@gmail.com>
2017-02-21 16:40:46 +0100

    Added war deployment options

--- src/main/java/com/github/life/LifeBackendApplication.java
+++ src/main/java/com/github/life/LifeBackendApplication.java
@@ -2,11 +2,18 @@ package com.github.life;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LifeBackendApplication {
+public class LifeBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LifeBackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LifeBackendApplication.class);
 	}
 }
